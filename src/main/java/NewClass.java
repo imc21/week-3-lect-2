eec41b9155768ffe9d7793584f9ff29432089b26
Caleb Im <imcy@farmingdale.edu>
2021-09-15 10:43:06 -0400

    3nd commit -added method for array element sums

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -13,11 +13,37 @@ public class NewClass {
          return y;
      }
     
+     //task 2: create a method that takes an integer array and returns
+     //the sum of the elements
+     
+     int getArrSum(int arr[]){
+         int sum = 0;
+         for(int i=0; i<arr.length; i++)
+         {
+             sum += arr[i];
+         }
+         return sum;
+             
+     }
+     
+     
     public static void main(String[] args){
         NewClass nc = new NewClass();
         String x = "Caleb Im";
        int len = nc.getStrLength(x);
        System.out.println("The length is " + len);
+       
+       int arrone[];
+       arrone = new int[5];
+       arrone[0] = 1;
+       arrone[1] = 2;
+       arrone[2] = 3;
+       arrone[3] = 4;
+       arrone[4] = 5;
+       int arrsum = nc.getArrSum(arrone);
+       System.out.println("The sum of the array elements is " + arrsum);
+       
+       
        
     }
     
