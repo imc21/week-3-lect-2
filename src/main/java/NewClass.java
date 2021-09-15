2deac91537f70d44e51acecc1ecdc12e6b402bcb
Caleb Im <imcy@farmingdale.edu>
2021-09-15 10:17:28 -0400

    init commit -project created

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,18 @@
+
+
+
+
+public class NewClass {
+    
+    int getSum(int a, int b){
+        return a+b;
+    }
+    
+    public static void main(String[] args){
+        NewClass nc = new NewClass();
+        int x=10, y = 100;
+        int z = nc.getSum(x,y);
+        System.out.println("The sum is " + z);
+    }
+    
+}
